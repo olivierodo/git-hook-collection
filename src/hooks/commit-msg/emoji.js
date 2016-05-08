8077b0afdc404a6b3626c3fc4c9856de54ecb861
Rodomond Olivier <rodomond.o@gmail.com>
2016-05-08 10:06:26 +0700

    facto emoji hook

--- src/hooks/commit-msg/emoji.js
+++ src/hooks/commit-msg/emoji.js
@@ -2,36 +2,30 @@
 module.exports = function(msg, s) {
   var emo = '',
   list = {
-    'fix' : 'wrench',
-    'add':'gift',
-    'added':'gift',
-    'update':'muscle',
-    'updated':'muscle',
-    'change':'muscle',
-    'improve' : 'lipstick',
-    'write' : 'memo',
-    'doc' : 'memo',
-    'remove' : 'fire',
-    'secure' : 'lock',
-    'upgrading' : 'arrow_up',
-    'downgrading' : 'arrow_down',
-    'conflict' : 'boom',
-    'merge' : 'seedling',
-    'test' : 'white_check_mark',
-    'refactor':'sunglasses',
-    'patch' : 'gift_heart',
-    'revert' : 'back',
-    'stop' : 'bangbang',
-    'perf' : 'racehorse',
-    'missing' : 'ghost',
-    'move' : 'rocket',
-    'moving' : 'rocket',
-    'link' : 'link',
-    'linking' : 'link',
+    'wrench' : ['fix'],
+    'gift' : ['add','added'],
+    'muscle' : ['update', 'updated', 'change'],
+    'lipstick' : ['improve'],
+    'memo' : ['write', 'doc'],
+    'fire' : ['remove'],
+    'lock' : ['secure'],
+    'arrow_up' : ['upgrading'],
+    'arrow_down' : ['downgrading'],
+    'boom' : ['conflict'],
+    'seedling' : ['merge'],
+    'white_check_mark' : ['test'],
+    'sunglasses' : ['refactor'],
+    'gift_heart' : ['patch'],
+    'back' : ['revert'],
+    'bangbang' : ['stop'],
+    'racehorse' : ['perf'],
+    'ghost' : ['missing'],
+    'rocket' : ['move', 'moving'],
+    'link' : ['link', 'linking'],
   };
   Object.keys(list).forEach(function (key) {
-    if (msg.trim().split(' ')[0].toLowerCase() === key)
-      emo = [':',list[key],':'].join('');
+    if ( -1 !== list[key].indexOf(msg.trim().split(' ')[0].toLowerCase()))
+      emo = [':',key,':'].join('');
   });
   return [emo, msg].join(' ').trim();
 };
